a31b18d02dd37860a6f90abca388bf08b917f02e
josephjoel3099 <josephjoel3099@gmail.com>
2025-04-22 20:34:45 +0100

    refactor: new start

--- education/university_of_glasgow.py
+++
@@ -1,7 +0,0 @@
-class UniversityOfGlasgow:
-    """
-    @brief Master of Science in Robotics & AI
-    @date September 2022 – September 2023
-    @details Classification: With Merit
-    """
-    pass
--- html/namespaces_dup.js
+++
@@ -1,6 +0,0 @@
-var namespaces_dup =
-[
-    [ "main", "namespacemain.html", [
-      [ "main", "namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88", null ]
-    ] ]
-];
