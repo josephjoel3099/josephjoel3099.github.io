b2fd1d3a848354f6e866cd2eeb1fb5d751060cd7
joseph <joseph@kingdom.garden>
2025-04-26 11:06:28 +0100

    feat: logo

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "josephjoel", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
